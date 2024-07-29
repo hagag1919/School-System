9af4c11b934dff3f183f00a214db7e8abd41fb0d
Ahmed Hagag <100851728+hagag1919@users.noreply.github.com>
2024-07-29 17:36:55 +0300

    update project structure

---
+++ app/src/main/java/lab3/model/Student.java
@@ -0,0 +1,20 @@
+package lab3.model;
+
+public class Student {
+    private long id;
+    private String name;
+
+    private int level;
+
+    private int age;
+
+   
+
+    public Student(long id, String name, int level, int age) {
+        this.id = id;
+        this.name = name;
+        this.level = level;
+        this.age = age;
+    }
+
+}
